b62523d7ef1d1ec9d69f6fff339c0966c71516bd
Zakiya <zakiyamanzoor04@gmail.com>
2025-06-05 21:18:36 +0530

    add hero section of app

---
+++ src/components/hero.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { motion } from 'framer-motion';
+import { Button } from '@/components/ui/button';
+
+export default function Hero(){
+    return(
+        <div>
+
+        </div>
+    )
+}
